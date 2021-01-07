0c7c3caa192fa2a518da279ffcce23ff1e3034a7
Didier Crespo <dj.crespo.castilla@gmail.com>
2021-01-06 18:55:44 -0600

    created the operations

---
+++ js/program.js
@@ -0,0 +1,15 @@
+let suma = function(x, y){
+    return x+y;
+}
+
+let resta = function(x, y){
+    return x-y;
+}
+
+let division = function(x, y){
+    return x/y;
+}
+
+let multi = function(x, y){
+    return x*y;
+}
